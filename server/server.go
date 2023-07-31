fa38d2e6f5aa383c6f999591cae07caecc5e3cf8
Atul Bhatt <atulbhatt61@gmail.com>
2023-07-31 13:21:41 +0530

    server modified

--- server/server.go
+++ server/server.go
@@ -10,6 +10,33 @@ import (
 	"os"
 )
 
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	http.HandleFunc("/lissajous", func(w http.ResponseWriter, r *http.Request) {
+		lissajous(w, r)
+	})
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the Path component of the request URL r.
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+
+	fmt.Println(r.URL.Path)
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
 var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
 
 const (
